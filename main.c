382b901e79823e11564e0534b7249db2ce96ea93
Hüseyin <iletisim@huseyinyigit.com.tr>
2021-01-26 14:07:49 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,43 @@
+// crt_getcwd.c
+// Compile with: cl /W4 crt_getcwd.c
+// This program places the name of the current directory in the
+// buffer array, then displays the name of the current directory
+// on the screen. Passing NULL as the buffer forces getcwd to allocate
+// memory for the path, which allows the code to support file paths
+// longer than _MAX_PATH, which are supported by NTFS.
+
+#ifdef _WIN32
+#include <direct.h> // _getcwd
+#include <stdlib.h> // free, perror
+#include <stdio.h>  // printf
+#include <string.h> // strlen
+
+int main( void )
+{
+   char* buffer;
+
+   // Get the current working directory:
+   if ( (buffer = _getcwd( NULL, 0 )) == NULL )
+      perror( "_getcwd error" );
+   else
+   {
+      printf( "%s \nLength: %zu\n", buffer, strlen(buffer) );
+      free(buffer);
+   }
+}
+
+char* get_cwd()
+{
+  char *buf;
+  if ((buf = _getcwd(NULL, 0)) == NULL){
+    perror("_getcwd error");
+    return NULL;
+  }
+  else
+  {
+    return buf;
+    free(buf);
+  }
+}
+
+#endif
